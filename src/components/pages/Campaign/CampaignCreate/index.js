c1551d6bdc786f8e1bcde46d03d82e613e788098
julien <dev.guilbaud@gmail.com>
2023-08-13 09:50:43 +0200

    test2 campagne create

--- src/components/pages/Campaign/CampaignCreate/index.js
+++ src/components/pages/Campaign/CampaignCreate/index.js
@@ -2,6 +2,7 @@ import { useState } from "react";
 import { Footer } from "../../../Footer";
 import { Header } from "../../../Header";
 import { Main } from "../../../Main";
+import { Link } from "react-router-dom";
 
 export function CampaignCreate() {
     const [formAlert, setFormAlert] = useState(false)
